baf985fc2c4d7438cccec1f09487b417f3d7fc71
Simran Vinodkumar Ahirwar <70149684+simran0809@users.noreply.github.com>
2024-12-13 15:21:56 +0530

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,37 @@
+cars1 = "BMW"
+cars2 = "Ferrai" 
+cars3 = "mc laren"
+print(cars1[0])
+
+
+cars = ["Ford", "Volvo", "BMW"]
+
+cars[0] = "Toyota"
+
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+
+x = len(cars)
+
+print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+
+for x in cars: #### lines se print hoge
+  print(x)
+  
+#   You can use the append() method to add an element to an array.
+
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.append("Honda")  ### add the elements
+
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.pop(1)
+
+print(cars)
+
